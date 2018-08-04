79896c6cb55d1067672a6d7b82288aeb2f89cee9
kdilot <kdilot@gmail.com>
2018-08-03 17:52:58 -0700

    default

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
+import { BrowserRouter } from 'react-router-dom';
+
+import { createStore } from 'redux';
+import reducers from './reducers';
+import { Provider } from 'react-redux';
+
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+  , document.getElementById('root'));
+registerServiceWorker();
